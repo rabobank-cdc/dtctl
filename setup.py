d1281ac3055d2a87c8f184c80c17c6d01c5149e5
daanwa <daan@vynder.io>
2019-08-06 15:44:24 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # mandatory
     name='dtctl',
     # mandatory
-    version='0.2.1',
+    version='0.2.2',
     # mandatory
     author_email='daan@vynder.io',
     packages=find_packages(),
