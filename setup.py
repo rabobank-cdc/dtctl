9d732ad0944fd9cbfa2abd4a9535000fcbc4824b
daanwa <daan@vynder.io>
2019-09-25 13:01:44 +0200

    Small fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # mandatory
     name='dtctl',
     # mandatory
-    version='0.2.6',
+    version='0.2.7',
     # mandatory
     author_email='daan@vynder.io',
     packages=find_packages(),
