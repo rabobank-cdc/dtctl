6223c03077e4a6abc111bed5b4ee625c51b1fd5f
daanwa <daan@vynder.io>
2019-10-08 16:02:45 +0200

    Version bump because of changes and significant new features

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # mandatory
     name='dtctl',
     # mandatory
-    version='0.2.8',
+    version='0.3.0',
     # mandatory
     author_email='daan@vynder.io',
     packages=find_packages(),
